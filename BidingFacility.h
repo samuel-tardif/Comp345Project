81d7b9200fa06b6d0247749bccfd62b665d75cb8
Samuel Tardif <samhockey04@hotmail.com>
2021-03-28 12:58:38 -0400

    Comments + fixes on biding facility

--- BidingFacility.h
+++ BidingFacility.h
@@ -9,16 +9,16 @@ For COMP 345 -Assignment 1
 
 #pragma once
 
+//Includes
 #include <iostream>
 #include <map>
 #include <conio.h>
-
 #include "Player.h"
-
 
 
 using namespace std;
 
+//Main class for this portion of the work
 class BidingFacility {
 public:
 	static map<Player*, int>* bidMap;
@@ -38,16 +38,22 @@ public:
 	//Destructor
 	~BidingFacility();
 
+	//To assign a player to the biding facility
 	void changeOwner(Player* newPlayer);
 
+	//Gives player his coins
+	//and asks for bid
 	void makeBid();
 	
+	//Returns pllayer assigned to facility
 	Player* getOwner() const;
 
+	//Determines who wins the bid
 	static Player* resolveBids();
 
 private:
 
+	//Player assigned to biding facility
 	Player* owner;
 	
 };
