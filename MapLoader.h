4d4d222b947605937112ac2bff8d028b7c73a545
itawakol <57954077+itawakol@users.noreply.github.com>
2021-03-29 03:41:39 +0200

    Update MapLoader.h

--- MapLoader.h
+++ MapLoader.h
@@ -1,23 +1,44 @@
 #pragma once
+
 #include <iostream>
+#include <map>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <array>
+#include <cstdlib>
+#include "Map.h"
 using namespace std;
 
-struct Node {
-	int src, dest;
-};
-class Graph {
-public:
-	vector<vector<int>> adjList;
-	Graph(vector<Node> const& edges, int N);
-	void printGraph(Graph const& graph, int N);
 
-};
-
-struct MapLoader {
+class MapLoader {
 public:
-	int readFile(string filename);
-	int a, b, c, N;
+	//Default constructor
+	MapLoader();
+
+
+	//Copy constructor
+	MapLoader(const MapLoader& copied);
+	//operator overloads =
+	MapLoader& operator= (const MapLoader& bF);
+
+	//operator overloads <<
+	friend std::ostream& operator << (std::ostream& output, const MapLoader& m);
+
+	//Destructor
+	~MapLoader();
+
+	string* getFileName() const;
+	Map* getMap() const;
+	void setMap(Map);
+	void setFileName(string);
+
+
+	void GenerateMap();
+
+
+private:
+
+	Map* m;
+	string* filename;
 };
